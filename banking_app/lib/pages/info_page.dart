e6f0c14c6b5c88ed5b36b9baa465fd65e5ed60de
Animesh Shukla <animeshshukla1518@gmail.com>
2022-10-10 17:36:25 +0530

    added money adding feature

--- banking_app/lib/pages/info_page.dart
+++ banking_app/lib/pages/info_page.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class InfoPage extends StatelessWidget {
-  const InfoPage({Key? key}) : super(key: key);
+class InfoPage extends StatefulWidget {
+  @override
+  State<InfoPage> createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  double amt = 100000000.00;
+
+  TextEditingController amtcontroller = TextEditingController();
+
+  add_remove_Money(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: amtcontroller,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter ammount to be added',
+                    ),
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      amt += double.parse(amtcontroller.text);
+                      Navigator.of(context).pop();
+                    });
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return const AlertDialog(
+                            title: Text("Success"),
+                            titleTextStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                                fontSize: 20),
+                            backgroundColor: Colors.greenAccent,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            content: Text("Money Transfred"),
+                          );
+                        });
+                  },
+                  icon: const Icon(Icons.attach_money),
+                  label: const Text('ADD'),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    amtcontroller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    double amt = 100000000.00;
     final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
@@ -61,7 +122,9 @@ class InfoPage extends StatelessWidget {
           ClipRRect(
             borderRadius: BorderRadius.circular(20),
             child: TextButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                add_remove_Money(context);
+              },
               icon: const Icon(
                 Icons.monetization_on,
                 color: Colors.purple,
