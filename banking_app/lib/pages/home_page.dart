f96dd3355a10f56bedf1d990ed9662756f110170
Animesh Shukla <animeshshukla1518@gmail.com>
2022-10-10 12:54:01 +0530

    small bug fix

--- banking_app/lib/pages/home_page.dart
+++ banking_app/lib/pages/home_page.dart
@@ -10,61 +10,63 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            SizedBox(
-              height: size.height * 0.4,
-              child: Image.asset('assets/welcome.png'),
-            ),
-            SizedBox(
-              height: size.height * 0.1,
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: SizedBox(
-                  height: size.height * 0.1,
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.purple,
-                    width: size.width * 0.7,
-                    child: Text(
-                      'View All Coustomers',
-                      style: TextStyle(
-                        fontFamily: GoogleFonts.ubuntu().fontFamily,
-                        fontSize: 20,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: size.height * 0.4,
+                child: Image.asset('assets/welcome.png'),
+              ),
+              SizedBox(
+                height: size.height * 0.1,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: SizedBox(
+                    height: size.height * 0.1,
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: Colors.purple,
+                      width: size.width * 0.7,
+                      child: Text(
+                        'View All Coustomers',
+                        style: TextStyle(
+                          fontFamily: GoogleFonts.ubuntu().fontFamily,
+                          fontSize: 20,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: size.height * 0.02,
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: SizedBox(
-                  height: size.height * 0.1,
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.purple,
-                    width: size.width * 0.7,
-                    child: Text(
-                      'Add a coustomer',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: GoogleFonts.ubuntu().fontFamily,
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: SizedBox(
+                    height: size.height * 0.1,
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: Colors.purple,
+                      width: size.width * 0.7,
+                      child: Text(
+                        'Add a coustomer',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: GoogleFonts.ubuntu().fontFamily,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
